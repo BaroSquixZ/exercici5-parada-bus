b0517f5cd1d0e3774768b1e13283d02e51da77ac
Pol Reig <polreigbroto@gmail.com>
2021-06-21 20:34:06 +0200

    Return Error si no existe la linea

---
+++ src/components/FormNumParada.js
@@ -0,0 +1,50 @@
+export const FormNumParada = (props) => {
+  const { setError, bus, setBus } = props;
+  const urlAPI = "https://api.tmb.cat/v1/ibus/stops/2775?";
+  const appId = "b3956491";
+  const appKey = "b7af445677be260f52d3d02c5c4bc9de";
+
+  const tmbAPI = {
+    status: "success",
+    data: {
+      ibus: [
+        {
+          routeId: "0230",
+          line: "23",
+          "text-ca": "8 min",
+          "t-in-s": 520,
+          destination: "Parc Logístic",
+          "t-in-min": 8,
+        },
+      ],
+    },
+  };
+
+  const comprobarEstado = (e) => {
+    const linea = e.target.value;
+    const response = tmbAPI;
+    if (!response.ok) {
+      setError(true);
+      return;
+    }
+    if (response.status === "success") {
+      const busLinea = response.data.ibus.find(
+        (busLinea) => busLinea.line === linea
+      );
+      setBus(busLinea);
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="num-parada">Parada nº: </label>
+      <input
+        type="number"
+        id="num-parada"
+        value={bus.line}
+        onChange={(e) => comprobarEstado(e)}
+      />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+};
